6e95ae07e43a4ade398c2a22e84a9a8a328ac757
Devin Droddy <devin.droddy@gmail.com>
2025-05-16 10:00:49 -0400

    trig test

---
+++ tests/trig.rs
@@ -0,0 +1,20 @@
+use sci_calc::cordic;
+
+fn radians(degrees: f64) -> f64 {
+    degrees * std::f64::consts::PI / 180.0
+}
+
+#[test]
+fn precision() {
+    for theta in (-90..=90).step_by(15).map(f64::from) {
+        let (cos, sin) = cordic(radians(theta));
+        let cos_expected = radians(theta).cos();
+        let sin_expected = radians(theta).sin();
+
+        dbg!(theta, cos, cos_expected, sin, sin_expected);
+
+        // Difference is greater than 1e-13
+        assert!((cos - cos_expected).abs() < 1e-12);
+        assert!((sin - sin_expected).abs() < 1e-12);
+    }
+}
